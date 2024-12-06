36f8c830695052b612898a5be4bb37047e16d5cf
Kurt337 <xui.228.338.ya ebal.05@gmail.com>
2024-12-07 01:47:27 +0300

    создал функцию со списком треков для вывода трека дня

---
+++ tracks.py
@@ -0,0 +1,21 @@
+import random
+
+tracks = [
+    "HILLS? - KAI ANGEL",
+    "БУДЬ ЧТО БУДЕТ - dekma(LOX)",
+    "ИЗНАСИЛУЙ МЕНЯ - zavet"
+    "HASHIRA - KAI ANGEL"
+    "ОГНЕЙ - SALUKI"
+    "Не Одиноко - GONE.FLUDD"
+    "Tusa V.I.P. - 9mice, KAI ANGEL"
+    "FLAГИ ИBANЫE 19.7.23ъ - JDFLAG"
+    "PARIS 2008 - KAI ANGEL"
+    "2017 - 9mice, KAI ANGEL"
+    "double rr - doxxxelll"
+    "SHOW OFF - KAI ANGEL"
+    "АНТИГЕРОЙ - КРЕСТ"
+]
+
+def get_track():
+    """Возвращает случайный трек из списка."""
+    return random.choice(tracks)
